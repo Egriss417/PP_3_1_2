da41359308d25640095e8f857579b793a21be18f
Egriss417 <73225068+Egriss417@users.noreply.github.com>
2024-08-11 18:15:16 +0300

    Update RoleServiceImp.java
    delete @EnableTransactionManagement

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImp.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@EnableTransactionManagement
 public class RoleServiceImp implements RoleService {
     private final RoleDao roleDao;
 
